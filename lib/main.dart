7a615d1830a68084a0209c220c340c227ddc8d82
Petar Cevriz <pcevriz95@gmail.com>
2021-06-09 13:49:16 +0200

    Show dummy products, add some basic style

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './screens/product_overview_screen.dart';
+
 void main() => runApp(App());
 
 class App extends StatelessWidget {
@@ -7,14 +9,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'My Flutter App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Home'),
-        ),
-        body: Center(
-          child: Text('Welcome!'),
-        ),
-      ),
+      home: ProductOverviewScreen(),
     );
   }
 }
